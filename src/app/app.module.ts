f1aa5fb75a1469034ea4efd5bb88b215e8b565f1
JeremyT313 <xdetroit@live.com>
2020-07-06 22:41:52 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SkillsComponent } from './skills/skills.component';
+import { MainComponent } from './main/main.component';
+import { EducationComponent } from './education/education.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NavbarComponent, SkillsComponent, MainComponent, EducationComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
