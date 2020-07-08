729bb055b174e12f2868265884e94324b7d6cbdb
JeremyT313 <xdetroit@live.com>
2020-07-08 19:34:17 -0400

    deleted all typescript in main component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { resolveSanitizationFn } from '@angular/compiler/src/render3/view/template';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
 export class MainComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
